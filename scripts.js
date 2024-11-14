57722656559777cfb4aa74000b6587f90f1dec48
SaiVenkatesh999 <saivenkatesh.asv@gmail.com>
2024-11-14 12:18:01 -0600

    v3a

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Simple script to show alert when someone clicks "Contact Me"
 document.querySelector("#contact").addEventListener("click", function () {
   alert(
-    "Thanks for reaching out! Feel free to email me at saivenkatesh.asv.com."
+    "Thanks for reaching out! Feel free to email me at saivenkatesh.asv@gmail.com."
   );
 });
